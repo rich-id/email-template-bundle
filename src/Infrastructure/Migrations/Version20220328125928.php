7e943fa83f2003485c10a9da5653023fc7ee8aea
Matthias Devlamynck <matthias.devlamynck@digiforma-certif.com>
2025-10-02 15:11:28 +0200

    Make migrations non transactional as mysql does not support schema changes in transactions

--- src/Infrastructure/Migrations/Version20220328125928.php
+++ src/Infrastructure/Migrations/Version20220328125928.php
@@ -9,6 +9,11 @@ use Doctrine\Migrations\AbstractMigration;
 
 final class Version20220328125928 extends AbstractMigration
 {
+    public function isTransactional(): bool
+    {
+        return false;
+    }
+
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE module_email_template_configuration (id INT AUTO_INCREMENT NOT NULL, slug VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_E8B2DE8A989D9B62 (slug), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE `UTF8_unicode_ci` ENGINE = InnoDB');
